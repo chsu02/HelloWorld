ebcb5d2f27c2849f86cbce4cb667e63f14f85cf9
Alex <alexhsu@hotmail.com>
2014-03-24 19:51:01 -0700

    Change Text to (in Master Branch)

--- src/Hello.java
+++ src/Hello.java
@@ -5,6 +5,6 @@ public class Hello {
 	 * @param arguments
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello, world");
+		System.out.println("Hello, world in master branch");
 	}
 }
